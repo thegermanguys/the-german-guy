0169407eec4490126e70c4d49948c150e9e534d6
Awanish Srivastava <40716261+imawanish5@users.noreply.github.com>
2026-04-08 16:19:43 +0200

    Update page.js

--- app/partnered-universities/page.js
+++ app/partnered-universities/page.js
@@ -146,7 +146,7 @@ export default function PartneredUniversities() {
 
           <UniversityCard
             name="University of Applied Sciences Europe"
-            logo="/assets/IC-UE-Logo.webp.png"
+            logo="/assets/IC-UE-Logo.png"
             formLink="https://docs.google.com/forms/d/e/1FAIpQLSc8HMuqwMJpLGNJ6HjfUa1PkkLFr1kLfV3oQD6HNeffXp3zQA/viewform"
           />
 
